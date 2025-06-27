da213e01d49a8d55c3d18b9848a689a0f687a191
Your Name <you@example.com>
2025-06-27 17:37:05 +0200

    Update the Post API with content and images for new desserts

--- server.js
+++ server.js
@@ -14,10 +14,36 @@ app.get("/api/posts", (req, res) => {
     const posts = [
         {
             title: "Ciambellone",
-            content: "questo è un ciambellone",
+            content: "Il dolce più richiesto",
             image: `http://localhost:${port}/images/ciambellone.jpeg`,
             tags: ["dolce", "cucina", "dessert"]
+        },
+
+        {
+            title: "Cracker di barbabietola",
+            content: "Cracker a base di barbabietola.",
+            image: `http://localhost:${port}/images/cracker_barbabietola.jpeg`,
+            tags: ["cracker", "snack", "barbabietola"]
+        },
+        {
+            title: "Pane fritto dolce",
+            content: "Pane fritto per colazione",
+            image: `http://localhost:${port}/images/pane_fritto_dolce.jpeg`,
+            tags: ["dolce", "colazione", "tradizione"]
+        },
+        {
+            title: "Pasta di barbabietola",
+            content: "Pasta a base di barbabietola",
+            image: `http://localhost:${port}/images/pasta_barbabietola.jpeg`,
+            tags: ["pasta", "barbabietola", "pranzo"]
+        },
+        {
+            title: "Torta paesana",
+            content: "Un dolce molto famoso",
+            image: `http://localhost:${port}/images/torta_paesana.jpeg`,
+            tags: ["torta", "dolce", "tradizione"]
         }
+
     ]
 
     res.json(posts)
